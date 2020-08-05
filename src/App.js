e822bbbeb05f96d9a63c1df77f3ecd4cfab14545
Lucidreline <techservs@pm.me>
2020-08-05 15:12:16 -0700

    Added a section title component.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 
 // sections
 import Hero from './components/sections/hero/hero.component';
+import Events from './components/sections/events/events.component';
 
 function App() {
   return (
     <div className='App'>
       <Hero />
+      <div id='container'>
+        <Events />
+      </div>
     </div>
   );
 }
---
+++ src/components/sections/events/events.component.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SectionTitle from '../../section-title/section-title.component';
+
+class Events extends Component {
+  render() {
+    return (
+      <div>
+        <SectionTitle title='Events' />
+      </div>
+    );
+  }
+}
+
+export default Events;
