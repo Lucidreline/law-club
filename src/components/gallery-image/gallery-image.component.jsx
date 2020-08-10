9a93729d60e6fe1fd91e8692dc21961dc36ed3d6
Lucidreline <techservs@pm.me>
2020-08-09 20:40:07 -0700

    Gallery now has new images. Captions are now optional by leaving them blank (the caption area of GalleryImage will disapear.

--- src/components/gallery-image/gallery-image.component.jsx
+++ src/components/gallery-image/gallery-image.component.jsx
@@ -3,15 +3,21 @@ import React from 'react';
 import './gallery-image.styles.scss';
 
 const GalleryImage = ({ imageUrl, caption, index }) => {
+  let captionSection;
+  caption.trim() !== ''
+    ? (captionSection = (
+        <div className='caption'>
+          <p>{caption}</p>
+        </div>
+      ))
+    : (captionSection = null);
   return (
     <div
-      className={`gallery-image`}
+      className='gallery-image'
       id={`image-${index}`}
       style={{ backgroundImage: `url(${imageUrl})` }}
     >
-      <div className='caption'>
-        <p>{caption}</p>
-      </div>
+      {captionSection}
     </div>
   );
 };
--- src/components/sections/gallery/gallery.component.jsx
+++ src/components/sections/gallery/gallery.component.jsx
@@ -13,12 +13,17 @@ class Gallery extends Component {
         {
           url:
             'https://res.cloudinary.com/dglxjd018/image/upload/q_auto:eco/v1596869345/pls/gallery/gallery_groupPicture_voez71.png',
-          caption: 'General body meeting selfie!',
+          caption: 'General body meeting group selfie!',
         },
         {
           url:
-            'https://shulman.ca/wp-content/uploads/2018/01/changes_to_family_law_ontario.jpg',
-          caption: 'A stock placeholder image.',
+            'https://res.cloudinary.com/dglxjd018/image/upload/v1597029227/pls/gallery/workingOnBoard2_sagdyg.jpg',
+          caption: 'Working on our bulletin board on campus!',
+        },
+        {
+          url:
+            'https://res.cloudinary.com/dglxjd018/image/upload/v1597029940/pls/gallery/workingOnBoard1_hf38t8.jpg',
+          caption: 'Group photo in front of our bulletin board!',
         },
       ],
       activeImage: {},
