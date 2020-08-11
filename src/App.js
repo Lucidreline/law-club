56e10952c8c930e6b003a369b613ee31ec86ee6e
Lucidreline <techservs@pm.me>
2020-08-10 23:05:00 -0700

    Added new members to board section. Started contact section.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Hero from './components/sections/hero/hero.component';
 import Events from './components/sections/events/events.component';
 import MeetTheBoard from './components/sections/meet-the-board/meet-the-board.component';
 import Gallery from './components/sections/gallery/gallery.component';
+import ContactUs from './components/sections/contact-us/contact-us.component';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Events />
         <MeetTheBoard />
         <Gallery />
+        <ContactUs />
       </div>
     </div>
   );
