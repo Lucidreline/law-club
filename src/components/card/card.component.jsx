06c04e4c45fe6bacd77495517ff0729b371ec87c
Lucidreline <techservs@pm.me>
2020-08-17 15:36:12 -0700

    Cards on HomePage are no longer links as a whole. There is a link inside the card to prevent acidently clicking it on mobile.

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -20,14 +20,14 @@ const Card = ({ data }) => {
       className='custom-card'
       style={{ backgroundImage: `url(${data.image})` }}
     >
-      <Link to={`${link}/${data.id}`}>
-        <div className='dark-hover'>
-          <div className='card-content'>
-            <h4 className='name'>{name}</h4>
-            <p className='link'>Click For More Info</p>
-          </div>
+      <div className='dark-hover'>
+        <div className='card-content'>
+          <h4 className='name'>{name}</h4>
+          <Link classname='link' to={`${link}/${data.id}`}>
+            <p className='link-text'>Click For More Info</p>
+          </Link>
         </div>
-      </Link>
+      </div>
     </div>
   );
 };
