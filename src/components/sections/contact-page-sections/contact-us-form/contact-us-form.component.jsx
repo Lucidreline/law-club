9b54dd9061073ad98fd8f76f1670bb946890be03
Lucidreline <techservs@pm.me>
2020-08-20 16:07:50 -0700

    Added additional styles to the contact form.

--- src/components/sections/contact-page-sections/contact-us-form/contact-us-form.component.jsx
+++ src/components/sections/contact-page-sections/contact-us-form/contact-us-form.component.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import Input from '../../../input/input.component';
 import Btn from '../../../btn/btn.component'
 
@@ -27,26 +28,55 @@ class ContactUsForm extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault()
-    e.target.reset()
+    const { name, email, message } = this.state
 
     // validate data
+
+
     // send via sendgrid
-    const { name, email, message } = this.state
-    const msg = {
-      name,
-      email,
-      message
-    }
-    console.log('sending: ' + msg.name, msg.email, msg.message)
+
+    this.sendEmail(name, email, message)
 
 
-    this.setState({ name: '', email: '', message: '' })
+    // clear form
+    this.clearForm(e)
+
   }
+
+  clearForm = (form) => {
+    this.setState({ name: '', email: '', message: '' })
+    form.target.reset()
+    document.querySelectorAll('.contact-us-form form input').forEach(input => {
+      input.classList.remove('valid')
+    })
+    document.querySelectorAll('.contact-us-form form textarea').forEach(input => {
+      input.classList.remove('valid')
+    })
+  }
+
+  sendEmail = (_name, _email, _message) => {
+
+    // const email = {
+    //   to: 'ma52castaneda@gmail.com',
+    //   from: 'ma52castaneda@gmail.com',
+    //   subject: 'Website Message',
+    //   text: `
+    //   From: ${_name}\n
+    //   Email: ${_email}\n
+    //   Message: ${_message}\n
+    //   `
+    // }
+
+    // send this as a post request to back ends
+  }
+
+
 
   render() {
     return (
       <div className='contact-us-form'>
         <form onSubmit={this.handleSubmit}>
+          <h2 className='form-title'>Send us an Email!</h2>
           <Input type='text' name='name' placeholder='Name' handleChange={this.handleChange} />
           <Input type='text' name='email' placeholder='Email' handleChange={this.handleChange} />
           <Input type='textArea' name='message' placeholder='Message' handleChange={this.handleChange} />
