1da6112942ab8f2e2ecb10ebdaa6b6c8c5fcb9d4
Lucidreline <techservs@pm.me>
2020-08-27 14:59:31 -0700

    Preparing app to run on production since it is now a full stack app.

---
+++ client/build/precache-manifest.478bd0cd3fd474574db396a4c8207ce2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56cf613c9178c45d206473910a75e9b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ce41d55f9f97ee56c36",
+    "url": "/static/css/2.5a9a206f.chunk.css"
+  },
+  {
+    "revision": "c8bfc024a93d1bd457e4",
+    "url": "/static/css/main.f3d5c8c5.chunk.css"
+  },
+  {
+    "revision": "2ce41d55f9f97ee56c36",
+    "url": "/static/js/2.3b310bd0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3b310bd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8bfc024a93d1bd457e4",
+    "url": "/static/js/main.67daf083.chunk.js"
+  },
+  {
+    "revision": "49b23250d012ecc46a29",
+    "url": "/static/js/runtime-main.1cb1002d.js"
+  }
+]);
