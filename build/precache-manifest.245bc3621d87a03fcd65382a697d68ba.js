efc478710787611a76a9375047b32c8acc9f19a1
Lucidreline <servs1@pm.me>
2020-08-28 01:50:58 +0000

    Changed .gitignore to keep the build file out of the repo.

---
+++ build/precache-manifest.245bc3621d87a03fcd65382a697d68ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0bb0923fb38cd6cfeb959d618a7b398",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2abe7574c3302e922d30",
+    "url": "/static/css/2.5a9a206f.chunk.css"
+  },
+  {
+    "revision": "e35ff9ba0c6c6c9b3471",
+    "url": "/static/css/main.a497d6c4.chunk.css"
+  },
+  {
+    "revision": "2abe7574c3302e922d30",
+    "url": "/static/js/2.9d6a6f3a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9d6a6f3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e35ff9ba0c6c6c9b3471",
+    "url": "/static/js/main.f9043687.chunk.js"
+  },
+  {
+    "revision": "49b23250d012ecc46a29",
+    "url": "/static/js/runtime-main.1cb1002d.js"
+  }
+]);
